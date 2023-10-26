bc274ee391b15aaaa4e38b482749f14906df2801
Henny2 <henrieke.baunack@web.de>
2023-10-25 18:19:49 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Henrieke Baunack on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
